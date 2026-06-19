41ed9ef7fca454bd934fc8bc5ded000a9297ed5b
Marie Glo Generalao <glomarie310@gmail.com>
2026-06-19 09:54:59 +0800

    Redesign header

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,22 +1,72 @@
-<div class="topbar text-white" style="background-color:#f7941d;">
-    <div class="container d-flex justify-content-between align-items-center py-2">
+<style>
+.topbar {
+    background-color: #f7941d;
+    font-size: 14px;
+}
 
-        <!-- Left Side -->
+.topbar-inner {
+    gap: 10px;
+}
+
+.topbar-right {
+    gap: 14px;
+}
+
+.navbar-brand img {
+    height: 90px;
+    width: auto;
+}
+
+@media (max-width: 768px) {
+    .topbar-inner {
+        flex-direction: column;
+        text-align: center;
+    }
+
+    .topbar-right {
+        flex-wrap: wrap;
+        justify-content: center;
+        gap: 8px;
+        font-size: 13px;
+    }
+
+    .topbar-separator {
+        display: none;
+    }
+
+    .navbar-brand img {
+        height: 60px;
+    }
+
+    .navbar-nav {
+        text-align: center;
+        padding-top: 15px;
+    }
+
+    .nav-link {
+        padding: 12px 0 !important;
+    }
+}
+</style>
+
+<div class="topbar text-white">
+    <div class="container d-flex justify-content-between align-items-center py-2 topbar-inner">
+
         <div class="fst-italic">
             The Official Website of the Local Government of Baliangao
         </div>
 
-        <!-- Right Side -->
-        <div class="d-flex align-items-center gap-3">
+        <div class="d-flex align-items-center topbar-right">
 
             <img src="{{ asset('images/ph-flag.png') }}"
-                alt="Philippine Flag"
-                height="18"
-                class="me-2">
-            <span>|</span>
+                 alt="Philippine Flag"
+                 height="18">
+
+            <span class="topbar-separator">|</span>
 
             <span id="datetime"></span>
-            <span>|</span>
+
+            <span class="topbar-separator">|</span>
 
             <a href="https://www.facebook.com/onesweet.asenso.baliangao/"
                target="_blank"
@@ -41,58 +91,57 @@
 </div>
 
 <nav class="navbar navbar-expand-lg bg-light shadow-sm">
-<div class="container">
+    <div class="container">
 
-<!-- Logo -->
-<a class="navbar-brand fw-bold d-flex align-items-center" href="/">
-<img src="/images/logo2.png" height="90" class="me-2">
+        <a class="navbar-brand fw-bold d-flex align-items-center" href="{{ url('/') }}">
+            <img src="{{ asset('images/logo2.png') }}" class="me-2" alt="Baliangao Logo">
+        </a>
 
-</a>
+        <button class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#nav"
+                aria-controls="nav"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-<!-- Mobile Toggle -->
-<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
-<span class="navbar-toggler-icon"></span>
-</button>
+        <div class="collapse navbar-collapse" id="nav">
 
-<div class="collapse navbar-collapse" id="nav">
+            <ul class="navbar-nav mx-auto text-uppercase fw-semibold">
 
-<!-- Center Menu -->
-<ul class="navbar-nav mx-auto text-uppercase fw-semibold">
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="{{ url('/know-baliangao') }}">Know Baliangao</a>
+                </li>
 
-<li class="nav-item">
-<a class="nav-link px-3" href="{{ url('/know-baliangao') }}">Know Baliangao</a>
-</li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="{{ url('/tourism') }}">Tourism</a>
+                </li>
 
-<li class="nav-item">
-<a class="nav-link px-3" href="{{ url('/tourism') }}">Tourism</a>
-</li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="{{ url('/offices') }}">Offices</a>
+                </li>
 
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="{{ url('/services') }}">Services</a>
+                </li>
 
-<li class="nav-item">
-<a class="nav-link px-3" href="{{ url('/offices') }}">Offices</a>
-</li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="{{ url('/transparency') }}">Transparency</a>
+                </li>
 
-<li class="nav-item">
-<a class="nav-link px-3" href="{{ url('/services') }}">Services</a>
-</li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="{{ url('/news') }}">News</a>
+                </li>
 
-<li class="nav-item">
-<a class="nav-link px-3" href="{{ url('/transparency') }}">Transparency</a>
-</li>
+                <li class="nav-item">
+                    <a class="nav-link px-3" href="{{ url('/contact') }}">Contact Us</a>
+                </li>
 
-<li class="nav-item">
-<a class="nav-link px-3" href="{{ url('/news') }}">News</a>
-</li>
+            </ul>
 
-<li class="nav-item">
-<a class="nav-link px-3" href="{{ url('/contact') }}">Contact Us</a>
-</li>
+        </div>
 
-
-</ul>
-
-</div>
-
-</div>
-</div>
+    </div>
 </nav>
